353e669bc6b274c084827b93cced49a465d3dad4
Anton-Mergaerts <anton.mergaerts@student.ucll.be>
2024-12-16 22:21:03 +0100

    Update

--- back-end/test/club.test.ts
+++ back-end/test/club.test.ts
@@ -1,21 +1,14 @@
-import test from "node:test";
 import { Club } from "../model/club";
 import { Region } from "../model/region";
 
 let validRegion = new Region(1, "Region 1");
 
-beforeAll(() => {
-})
-
-beforeEach(() => {
-})
-
 test('given valid values, when: club is created, then: club is created with those values', () => { 
-    // Exercise
+
     const club = new Club(1, validRegion, "Address 1");
 
-    // Verify
     expect(club.getId()).toBe(1);
     expect(club.getRegion()).toBe(validRegion);
     expect(club.getAddress()).toBe("Address 1");
+
 })
