b415e6e1d5cf60c04cc41d32d50dfceaa2c2e229
Anton-Mergaerts <anton.mergaerts@student.ucll.be>
2024-12-16 22:07:33 +0100

    Basic happy case for club

--- back-end/test/club.test.ts
+++ back-end/test/club.test.ts
@@ -1,8 +1,21 @@
 import test from "node:test";
 import { Club } from "../model/club";
+import { Region } from "../model/region";
 
+let validRegion = new Region(1, "Region 1");
+
+beforeAll(() => {
+})
+
+beforeEach(() => {
+})
 
 test('given valid values, when: club is created, then: club is created with those values', () => { 
-    const club = new Club("
-        
+    // Exercise
+    const club = new Club(1, validRegion, "Address 1");
+
+    // Verify
+    expect(club.getId()).toBe(1);
+    expect(club.getRegion()).toBe(validRegion);
+    expect(club.getAddress()).toBe("Address 1");
 })
