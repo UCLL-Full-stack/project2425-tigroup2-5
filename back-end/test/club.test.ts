904facea0b56c208af59e08f2ae410f021575941
Lennard Mergaerts <lennard.mergaerts@gmail.com>
2024-12-16 19:14:28 +0100

    initial commit

---
+++ back-end/test/club.test.ts
@@ -0,0 +1,8 @@
+import test from "node:test";
+import { Club } from "../model/club";
+
+
+test('given valid values, when: club is created, then: club is created with those values', () => { 
+    const club = new Club("
+        
+})
