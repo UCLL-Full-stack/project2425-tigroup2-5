296ab33bb5a81ba5ef60fd1c632437cc21f7ebf4
Lennard Mergaerts <lennard.mergaerts@gmail.com>
2024-12-20 21:36:58 +0100

    brol

--- front-end/src/app/components/subscription/subscriptionOverview.tsx
+++ front-end/src/app/components/subscription/subscriptionOverview.tsx
@@ -1,13 +1,37 @@
+import { Subscription } from "../../../../types";
 import Header from "../header";
 
-const SubscriptionOverview = () => {
+type Props = {
+    subscriptions: Array<Subscription>;
+};
+
+
+const SubscriptionOverview: React.FC<Props> = ({subscriptions}) => {
     return (
         <div>
-            <Header></Header>
             <div className="bg-white shadow-md rounded-lg p-6">
                 <h1 className="text-2xl font-bold mb-4 text-black">My Subscriptions</h1>
                 <div className="subscription-list">
-                    {/* Render the list of subscriptions here */}
+                    <div className="col-md-12">
+                        <table className="table table-striped">
+                                <thead className="text-black">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Type</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-black">
+                                    {subscriptions.map((subscription: Subscription) => (
+                                        <tr key={subscription.id}>
+                                            <td>{subscription.id}</td>
+                                            <td>{subscription.type}</td>
+                                            <td>{subscription.price}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                 </div>
             </div>
         </div>
--- front-end/src/app/pages/subscription/page.tsx
+++ front-end/src/app/pages/subscription/page.tsx
@@ -1,10 +1,34 @@
-import Header from "@/app/components/header";
-import SubscriptionOverview from "@/app/components/subscription/subscriptionOverview";
+"use client";
 
-export default function Subscription() {
+import SubscriptionOverview from "@/app/components/subscription/subscriptionOverview";
+import { Subscription } from "../../../../types";
+import { useEffect, useState } from "react";
+import SubscriptionService from "../../../../service/subscriptionService";
+import Header from "@/app/components/header";
+
+export default function SubscriptionPage() {
+
+    const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+    
+    useEffect(() => {
+        getSubscriptions();
+    }, []);
+    
+    const getSubscriptions = async () => {
+        const response = await SubscriptionService.getAllSubscriptions();
+        const data = await response.json();
+        setSubscriptions(data);
+    }
     return (
         <div>
-            <SubscriptionOverview></SubscriptionOverview>
+            <Header></Header>
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="bg-white p-6 rounded-lg shadow-lg">
+                    <h1 className="text-2xl text-gray-700 font-bold mb-2">Subscriptions</h1>
+                    <h2 className="text-lg text-gray-700">These are the subscriptions</h2>
+            <SubscriptionOverview subscriptions={subscriptions}></SubscriptionOverview>
+            </div>
+        </div>
         </div>
     );
 }
